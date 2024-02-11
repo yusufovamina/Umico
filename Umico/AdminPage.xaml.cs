05523bbbe1ab5af982eb918339c6f248ba23ac2e
Amina <yusufovamina@gmail.com>
2024-02-12 00:55:38 +0400

    commited

---
+++ Umico/AdminPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Umico
+{
+    /// <summary>
+    /// Interaction logic for AdminPage.xaml
+    /// </summary>
+    public partial class AdminPage : Page
+    {
+        public AdminPage()
+        {
+            DataContext = this;
+            InitializeComponent();
+            LoadOrderNames();
+        }
+
+
+
+        public void LoadOrderNames()
+        {
+            using (var dbContext = new AppContext())
+            {
+                var names = dbContext.Orders.Select(order => order.Name).ToList();
+                foreach (var name in names)
+                {
+                    OrdersNames.Items.Add(name);
+                }
+
+            }
+        }
+
+
+        private void AdminLogin_CLick(object sender, RoutedEventArgs e)
+        {
+            if (Username.Text != "admin" && Password.Password != "admin")
+            {
+                MessageBox.Show("Incorrect username or password. Try again!");
+            }
+            else if (Username.Text == "admin" && Password.Password == "admin")
+            {
+                Login.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void OrdersNames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            using (var db = new AppContext()) {
+                var c1 = db.Orders.Where(o => o.Name == OrdersNames.SelectedValue.ToString()).ToList();
+                CustomerName.Text = c1[0].Customer.Name;
+    } } }
+}
--- Umico/MainWindow.xaml.cs
+++ Umico/MainWindow.xaml.cs
@@ -30,13 +30,13 @@ namespace Umico
         {
             using (AppContext db = new AppContext())
             {
-                Customer customer1 = new Customer() { Name = "Afruz", Surname = "Quliyeva", Age = 21, Username="afruz21",Password="lalala12" };
-                Customer customer2 = new Customer() { Name = "Farid", Surname = "Salayev", Age = 19, Username="salayev",Password="12345678" };
-                Customer customer3 = new Customer() { Name = "Alina", Surname = "Mirzoyeva", Age = 17,Username="alina111",Password="2007alina" };
-                Customer customer4 = new Customer() { Name = "Rustam", Surname = "Veliyev", Age = 24, Username="veliyevrustam",Password="000111222" };
-                Customer customer5 = new Customer() { Name = "Leyla", Surname = "Dadasheva", Age = 24, Username="leyla_d", Password="87654321" };
-                Customer customer6 = new Customer() { Name = "Zaur", Surname = "Tagizade", Age = 24, Username="zaur01",Password="24242424" };
-                Customer customer7 = new Customer() { Name = "David", Surname = "Abdullayev", Age = 24, Username="abd_david",Password="daviddavid" };
+                Customer customer1 = new Customer() { Name = "Afruz", Surname = "Quliyeva", Age = 21, Username = "afruz21", Password = "lalala12" };
+                Customer customer2 = new Customer() { Name = "Farid", Surname = "Salayev", Age = 19, Username = "salayev", Password = "12345678" };
+                Customer customer3 = new Customer() { Name = "Alina", Surname = "Mirzoyeva", Age = 17, Username = "alina111", Password = "2007alina" };
+                Customer customer4 = new Customer() { Name = "Rustam", Surname = "Veliyev", Age = 24, Username = "veliyevrustam", Password = "000111222" };
+                Customer customer5 = new Customer() { Name = "Leyla", Surname = "Dadasheva", Age = 24, Username = "leyla_d", Password = "87654321" };
+                Customer customer6 = new Customer() { Name = "Zaur", Surname = "Tagizade", Age = 24, Username = "zaur01", Password = "24242424" };
+                Customer customer7 = new Customer() { Name = "David", Surname = "Abdullayev", Age = 24, Username = "abd_david", Password = "daviddavid" };
 
 
                 Product p1 = new Product() { Name = "Tomato", Price = 3 };
@@ -54,21 +54,40 @@ namespace Umico
                 Product p13 = new Product() { Name = "Carrot", Price = 5 };
 
 
-                Status status1 = new Status() { Name="Order is being processed" };
+                Status status1 = new Status() { Name = "Order is being processed" };
                 Status status2 = new Status() { Name = "Order was accepted" };
                 Status status3 = new Status() { Name = "Order was cancelled" };
                 Status status4 = new Status() { Name = "Order was delivered to Pick-Up point" };
                 Status status5 = new Status() { Name = "Order completed" };
 
 
-                db.Statuses.AddRange(status1,status2,status3,status4,status5);
-                db.Products.AddRange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13);
-                db.Customers.AddRange(customer1, customer2, customer3, customer4,customer5,customer6,customer7);
+                PickUpPoint pickup1 = new PickUpPoint() { Name = "PickUp 1", Location = "Nizami street 31" };
+                PickUpPoint pickup2 = new PickUpPoint() { Name = "PickUp 2", Location = "Ataturk prospekt 105 " };
+                PickUpPoint pickup3 = new PickUpPoint() { Name = "PickUp 3", Location = "Dilara Aliyeva street 78" };
+                PickUpPoint pickup4 = new PickUpPoint() { Name = "PickUp 4", Location = "Fizuli street 26" };
 
+                Order o1=new Order() { Name="Y45C3X09A2", Customer=customer1, PickUpPoint=pickup1, Products=new List<Product>() { p1,p2,p3}, Status=status1 };
+
+                //db.PickUpPoints.AddRange(pickup1, pickup2, pickup3, pickup4);
+                //db.Statuses.AddRange(status1,status2,status3,status4,status5);
+                //db.Products.AddRange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13);
+                //db.Customers.AddRange(customer1, customer2, customer3, customer4,customer5,customer6,customer7);
+                //db.Orders.Add(o1);
 
                 db.SaveChanges();
 
             }
+
+        }
+
+        private void AdminEnter_CLick(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new AdminPage();
+        }
+
+        private void MainFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+
         }
     }
 }
